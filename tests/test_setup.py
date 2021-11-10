b135089e5dd07313b0ddd059ee83ee219b3192c3
orioncohen <orion@lbl.gov>
2021-11-10 13:38:36 -0800

    skeleton for testing setup and simulations

---
+++ tests/test_setup.py
@@ -0,0 +1,36 @@
+import pytest
+
+
+
+@pytest.fixture
+def simulation_generator():
+
+@pytest.fixture
+def ethanol_water_smile_counts():
+
+
+
+def test_smile_to_mol():
+
+
+
+class TestOpenMMSimulationGenerator:
+
+    def test_smile_to_parmed_structure(self):
+
+
+
+    def test_smiles_to_openmm_topology(self):
+
+
+
+    def test_smiles_to_coordinates(self):
+
+
+    def test_smiles_to_cube_size(self):
+
+
+    def test_smiles_to_system_topology(self):
+
+
+    def test_return_simulation(self):
---
+++ tests/test_simulations.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.fixture
+def ethanol_simulation():
+    return
+
+def test_equilibrate_pressure():
+
+
+def test_anneal():
