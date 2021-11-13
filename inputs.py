fd7c49efe72a51c3dbe4b45230618e09915e73eb
orioncohen <orion@lbl.gov>
2021-11-12 16:29:57 -0800

    corrected format of InputFiles and updated the OpenMM input set

--- inputs.py
+++ inputs.py
@@ -25,7 +25,7 @@ __date__ = "Nov 2021"
 
 
 
-class TopologyInputFile(InputFile):
+class TopologyInput(InputFile):
     def __init__(self, topology: Topology, positions: Union[np.ndarray, List]):
         self.topology = topology
         self.positions = positions
@@ -41,9 +41,11 @@ class TopologyInputFile(InputFile):
     def from_string(cls, contents: str):
         with io.StringIO(contents) as s:
             pdb = PDBFile(s)
-        return pdb
+            topology = pdb.getTopology()
+            positions = pdb.getPositions(asNumpy=True)
+        return TopologyInput(topology, positions)
 
-class SystemInputFile(InputFile):
+class SystemInput(InputFile):
     def __init__(self, system: System):
         self.system = system
 
@@ -52,10 +54,10 @@ class SystemInputFile(InputFile):
 
     @classmethod
     def from_string(cls, contents: str):
-        return SystemInputFile(XmlSerializer.deserialize(contents))
+        return SystemInput(XmlSerializer.deserialize(contents))
 
 
-class IntegratorInputFile(InputFile):
+class IntegratorInput(InputFile):
     def __init__(self, integrator: Integrator):
         self.integrator = integrator
 
@@ -64,10 +66,10 @@ class IntegratorInputFile(InputFile):
 
     @classmethod
     def from_string(cls, contents: str):
-        return SystemInputFile(XmlSerializer.deserialize(contents))
+        return IntegratorInput(XmlSerializer.deserialize(contents))
 
 
-class StateInputFile(InputFile):
+class StateInput(InputFile):
     def __init__(self, state: State):
         self.state = state
 
@@ -76,14 +78,34 @@ class StateInputFile(InputFile):
 
     @classmethod
     def from_string(cls, contents: str):
-        return SystemInputFile(XmlSerializer.deserialize(contents))
+        return StateInput(XmlSerializer.deserialize(contents))
 
 
 
 class OpenMMSet(InputSet):
 
-    def from_directory(cls, directory: Union[str, Path]):
-        return
+
+    # TODO: if there is an optional file should it be missing or should the value be none?
+    @classmethod
+    def from_directory(cls,
+                       directory: Union[str, Path],
+                       topology_file: str = "topology.pdb",
+                       system_file: str = "system.xml",
+                       integrator_file: str = "integrator.xml",
+                       state_file:str = "state.xml",
+                       ):
+        topology = TopologyInput.from_file(topology_file)
+        system = TopologyInput.from_file(system_file)
+        integrator = TopologyInput.from_file(integrator_file)
+        openmm_set = OpenMMSet(
+            topology=topology,
+            system=system,
+            integrator=integrator,
+        )
+        if Path('state.xml').is_file():
+            state = TopologyInput.from_file(state_file)
+            openmm_set['state'] = state
+        return openmm_set
 
     def validate(self) -> bool:
         return False
