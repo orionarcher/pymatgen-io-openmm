7162d0a4938abbc172b312ac464a4811ed373f32
orioncohen <orion@lbl.gov>
2021-11-17 13:23:23 -0800

    add testing for OpenMMGenerator

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -1,4 +1,17 @@
+import pytest
 
+from pymatgen.io.openmm.inputs import (
+    TopologyInput,
+    SystemInput,
+    IntegratorInput,
+    StateInput,
+    OpenMMSet,
+    OpenMMGenerator,
+)
+
+import pymatgen
+import parmed
+import openmm
 
 class TestInputFiles:
     def test_topology_input(self):
@@ -27,12 +40,29 @@ class TestOpenMMSet:
 
 class TestOpenMMGenerator:
     def test_smile_to_molecule(self):
-        return
+        mol = OpenMMGenerator._smile_to_molecule("CCO")
+        assert isinstance(mol, pymatgen.core.structure.Molecule)
+        assert len(mol.sites) == 9
 
     def test_smile_to_parmed_structure(self):
-        return
+        struct1 = OpenMMGenerator._smile_to_parmed_structure("CCO")
+        assert isinstance(struct1, parmed.Structure)
+        assert len(struct1.atoms) == 9
+        assert len(struct1.residues) == 1
+        assert len(struct1.bonds) == 8
+        struct2 = OpenMMGenerator._smile_to_parmed_structure("O")
+        # currently failing because H2O is two residues. Issue raised on GitHub
+        # https://github.com/openbabel/openbabel/issues/2431
+        assert len(struct2.atoms) == 3
+        assert len(struct1.residues) == 2
+        assert len(struct1.bonds) == 2
 
     def test_get_openmm_topology(self):
+        topology = OpenMMGenerator._get_openmm_topology({"O": 200, "CCO": 20})
+        assert isinstance(topology, openmm.app.Topology)
+        assert topology.getNumAtoms() == 780
+        assert topology.getNumResidues() == 220
+        assert topology.getNumBonds() == 560
         return
 
     def test_get_box(self):
@@ -46,5 +76,3 @@ class TestOpenMMGenerator:
 
     def test_get_input_set(self):
         return
-
-
