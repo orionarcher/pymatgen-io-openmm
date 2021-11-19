f2c7515c232c609689f0a5061ef27679eeec977f
orioncohen <orion@lbl.gov>
2021-11-19 15:46:56 -0800

    added several test files and datafiles.py to organize test files

---
+++ tests/datafiles.py
@@ -0,0 +1,10 @@
+from pkg_resources import resource_filename
+import pathlib
+from pathlib import Path
+
+topology_path = resource_filename(__name__, "test_files/input_set_files/topology.pdb")
+system_path = resource_filename(__name__, "test_files/input_set_files/system.xml")
+integrator_path = resource_filename(__name__, "test_files/input_set_files/integrator.xml")
+state_path = resource_filename(__name__, "test_files/input_set_files/state.xml")
+
+coordinates_path = resource_filename(__name__, "test_files/coordinates.npy")
