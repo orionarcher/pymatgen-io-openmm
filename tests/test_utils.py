845fb7bd48f1d5df20d70413dd5c5f3dd6691933
orioncohen <orion@lbl.gov>
2021-12-16 14:11:48 -0800

    added utility functions for molarity, volume ratio, and mass ratio calculations.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,15 @@ import numpy as np
 import parmed
 import pymatgen
 
-from pymatgen.io.openmm.utils import get_box, smile_to_molecule, smile_to_parmed_structure
+from pymatgen.io.openmm.utils import (
+    get_box,
+    smile_to_molecule,
+    smile_to_parmed_structure,
+    n_mols_from_mass_ratio,
+    n_mols_from_volume_ratio,
+    n_solute_from_molarity,
+    calculate_molarity,
+)
 
 
 def test_get_box():
@@ -33,3 +41,26 @@ def test_smile_to_molecule():
     mol = smile_to_molecule("CCO")
     assert isinstance(mol, pymatgen.core.structure.Molecule)
     assert len(mol.sites) == 9
+
+
+def test_counts_from_mass_ratio():
+    n_mols = n_mols_from_mass_ratio(600, ["O", "CCO"], [10, 1])
+    np.testing.assert_allclose(n_mols, [577, 23])
+
+
+def test_n_mols_from_volume_ratio():
+    n_mols = n_mols_from_volume_ratio(600, ["O", "CCO"], [10, 1], [1, 0.79])
+    np.testing.assert_allclose(n_mols, [582, 18])
+
+
+def test_n_solute_from_molarity():
+    nm3_to_L = 1e-24
+    n_solute = n_solute_from_molarity(1, 2 ** 3 * nm3_to_L)
+    np.testing.assert_allclose(n_solute, 5)
+    n_solute = n_solute_from_molarity(1, 4 ** 3 * nm3_to_L)
+    np.testing.assert_allclose(n_solute, 39)
+
+
+def test_calculate_molarity():
+    nm3_to_L = 1e-24
+    np.testing.assert_almost_equal(calculate_molarity(4 ** 3 * nm3_to_L, 39), 1)
--- utils.py
+++ utils.py
@@ -38,15 +38,15 @@ def smile_to_molecule(smile: str) -> pymatgen.core.Molecule:
 
 def get_box(smiles: Dict[str, int], density: float) -> List[float]:
     """
-    Calculates the side_length of a cube necessary to contain the given molecules with
-    given density.
+    Calculates the dimensions of a cube necessary to contain the given molecules with
+    given density. The corner of the cube is at the origin.
 
     Args:
         smiles: keys are smiles and values are number of that molecule to pack
         density: guessed density of the solution, larger densities will lead to smaller cubes.
 
     Returns:
-        side_length: side length of the returned cube
+        dimensions: array of [0, 0, 0, side_length, side_length, side_length]
     """
     cm3_to_A3 = 1e24
     NA = 6.02214e23
@@ -57,3 +57,68 @@ def get_box(smiles: Dict[str, int], density: float) -> List[float]:
     box_volume = total_weight * cm3_to_A3 / (NA * density)
     side_length = round(box_volume ** (1 / 3), 2)
     return [0, 0, 0, side_length, side_length, side_length]
+
+
+def n_mols_from_mass_ratio(n_mol: int, smiles: List[str], mass_ratio: List[float]) -> np.ndarray[int]:
+    """
+    Calculates the number of mols needed to yield a given mass ratio.
+
+    Args:
+        n_mol: total number of mols. returned array will sum to n_mol. e.g. sum(n_mols) = n_mol.
+        smiles: a list of smiles. e.g. ["O", "CCO"]
+        mass_ratio: mass ratio of smiles. e.g. [9, 1]
+
+    Returns:
+        n_mols: number of each smile needed for mass ratio.
+    """
+    mols = [smile_to_molecule(smile) for smile in smiles]
+    mws = np.array([mol.composition.weight for mol in mols])
+    mol_ratio = np.array(mass_ratio) / mws
+    mol_ratio /= sum(mol_ratio)
+    return np.round(mol_ratio * n_mol)
+
+
+def n_mols_from_volume_ratio(
+    n_mol: int, smiles: List[str], volume_ratio: List[float], densities: List[float]
+) -> np.ndarray[int]:
+    """
+    Calculates the number of mols needed to yield a given volume ratio.
+
+    Args:
+        n_mol: total number of mols. returned array will sum to n_mol. e.g. sum(n_mols) = n_mol.
+        smiles: a list of smiles. e.g. ["O", "CCO"]
+        volume_ratio: volume ratio of smiles. e.g. [9, 1]
+        densities: density of each smile. e.g. [1, 0.79]
+
+    Returns:
+        n_mols: number of each smile needed for volume ratio.
+
+    """
+    mass_ratio = np.array(volume_ratio) * np.array(densities)
+    return n_mols_from_mass_ratio(n_mol, smiles, mass_ratio)
+
+
+def n_solute_from_molarity(molarity: float, volume: float) -> int:
+    """
+    Calculates the number of solute molecules needed for a given molarity.
+
+    Args:
+        molarity: molarity of solute desired.
+        volume: volume of box in liters.
+
+    Returns:
+        n_solute: number of solute molecules
+
+    """
+    NA = 6.02214e23
+    n_solute = volume * NA * molarity
+    return round(n_solute)
+
+
+def calculate_molarity(volume, n_solute):
+    """
+    Calculate the molarity of a number of solutes in a volume.
+    """
+    NA = 6.02214e23
+    molarity = n_solute / (volume * NA)
+    return molarity
