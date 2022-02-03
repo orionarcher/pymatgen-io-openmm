54bdacc94a48812282db4834573dd2bf81466389
orioncohen <orion@lbl.gov>
2022-02-02 17:55:38 -0800

    modified datafiles.py to support input set reorganization

--- pymatgen/io/openmm/tests/datafiles.py
+++ pymatgen/io/openmm/tests/datafiles.py
@@ -7,23 +7,23 @@ from pkg_resources import resource_filename  # type: ignore
 
 tests_path = Path(__file__).parent
 test_files_path = Path("test_files")
-input_set_path = test_files_path / "input_set"
+input_sets_path = test_files_path / "input_sets"
+default_input_set_path = input_sets_path / "default_input_set"
+alchemy_input_set_path = input_sets_path / "alchemy_input_set"
 partial_charges = test_files_path / "partial_charge"
 geometries = test_files_path / "geometries"
 
 
-topology_path = resource_filename(__name__, str(input_set_path / "topology.pdb"))
-system_path = resource_filename(__name__, str(input_set_path / "system.xml"))
-integrator_path = resource_filename(__name__, str(input_set_path / "integrator.xml"))
-state_path = resource_filename(__name__, str(input_set_path / "state.xml"))
-corrupted_state_path = resource_filename(
-    __name__, str(input_set_path / "corrupted_state.xml")
-)
-input_set_dir = resource_filename(__name__, str(input_set_path))
+topology_path = resource_filename(__name__, str(default_input_set_path / "topology.pdb"))
+system_path = resource_filename(__name__, str(default_input_set_path / "system.xml"))
+integrator_path = resource_filename(__name__, str(default_input_set_path / "integrator.xml"))
+state_path = resource_filename(__name__, str(default_input_set_path / "state.xml"))
+corrupted_state_path = resource_filename(__name__, str(default_input_set_path / "corrupted_state.xml"))
+input_set_dir = resource_filename(__name__, str(default_input_set_path))
 
-coordinates_path = resource_filename(
-    __name__, str(test_files_path / "water_ethanol_coordinates.npy")
-)
+alchemy_input_set_dir = resource_filename(__name__, str(alchemy_input_set_path))
+
+coordinates_path = resource_filename(__name__, str(test_files_path / "water_ethanol_coordinates.npy"))
 
 CCO_xyz = resource_filename(__name__, str(partial_charges / "CCO.xyz"))
 CCO_charges = resource_filename(__name__, str(partial_charges / "CCO.npy"))
