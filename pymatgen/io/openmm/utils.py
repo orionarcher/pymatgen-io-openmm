utils: fix units of nonbondedCutoff calc

pymatgen/io/openmm/utils.py
@@ -576,7 +576,9 @@ def parameterize_system(
             forcefield_omm.registerTemplateGenerator(template.generator)
 
     box_size = min(box[3] - box[0], box[4] - box[1], box[5] - box[2])
-    nonbondedCutoff = min(10, box_size // 2)
+    # NOTE: cutoff is in nm, not Angstrom!
+    # box_size is calculated in Angstrom. Convert to nm here
+    nonbondedCutoff = min(1, box_size // 20)
     # TODO: Make insensitive to input units
     periodic_box_vectors = np.array(
         [