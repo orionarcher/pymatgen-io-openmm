907d1100be349c03ded6882cd5b33ace9903adf4
orioncohen <orion@lbl.gov>
2023-03-11 12:55:43 -0800

    refactor conftests

--- pymatgen/io/openmm/tests/conftest.py
+++ pymatgen/io/openmm/tests/conftest.py
@@ -8,9 +8,10 @@ from pymatgen.io.openmm.alchemy_utils import AlchemicalReaction
 
 
 @pytest.fixture
-def select_dict():
+def acetic_ethanol_condensation():
     """
-    A fixture for a select dict for an acetic acid and ethanol condensation Alchemical Reaction.
+    A fixture for an acetic acid and ethanol condensation Alchemical Reaction where
+    the water is kept.
     """
     select_dict = {
         "acetic_acid_O": "smarts CC(=O)O and smarts C-O and element O",
@@ -19,15 +20,6 @@ def select_dict():
         "ethanol_O": "smarts [CH3][CH2]O and element O",
         "ethanol_H": "smarts [CH3][CH2]O[H] and smarts [OX2][H] and element H",
     }
-    return select_dict
-
-
-@pytest.fixture
-def acetic_ethanol_condensation(select_dict):
-    """
-    A fixture for an acetic acid and ethanol condensation Alchemical Reaction where
-    the water is kept.
-    """
     create_bonds = [("acetic_acid_C", "ethanol_O"), ("acetic_acid_O", "ethanol_H")]
     delete_bonds = [("ethanol_O", "ethanol_H"), ("acetic_acid_O", "acetic_acid_C")]
     rxn = AlchemicalReaction(
@@ -39,11 +31,41 @@ def acetic_ethanol_condensation(select_dict):
 
 
 @pytest.fixture
-def acetic_ethanol_condensation_del_water(select_dict):
+def acetic_ethanol_condensation_del_water(acetic_ethanol_select_dict):
     """
     A fixture for an acetic acid and ethanol condensation Alchemical Reaction where
     the water is deleted.
     """
+    select_dict = {
+        "acetic_acid_O": "smarts CC(=O)O and smarts C-O and element O",
+        "acetic_acid_H": "smarts CC(=O)O[H] and smarts [OX2][H] and element H",
+        "acetic_acid_C": "smarts CC(=O)O and smarts C=O and element C",
+        "ethanol_O": "smarts [CH3][CH2]O and element O",
+        "ethanol_H": "smarts [CH3][CH2]O[H] and smarts [OX2][H] and element H",
+    }
+    create_bonds = [("acetic_acid_C", "ethanol_O")]
+    delete_atoms = ["acetic_acid_O", "acetic_acid_H", "ethanol_H"]
+    rxn = AlchemicalReaction(
+        select_dict=select_dict,
+        create_bonds=create_bonds,
+        delete_atoms=delete_atoms,
+    )
+    return rxn
+
+
+@pytest.fixture
+def propanedioic_ethanol_condensation_del_water(acetic_ethanol_select_dict):
+    """
+    A fixture for an acetic acid and ethanol condensation Alchemical Reaction where
+    the water is deleted.
+    """
+    select_dict = {
+        "propanedioic_acid_O": "smarts OC(=O)CC(O)=O and smarts C-O and element O",
+        "propanedioic_acid_H": "smarts OC(=O)CC(O)=O and smarts [OX2][H] and element H",
+        "propanedioic_acid_C": "smarts OC(=O)CC(O)=O and smarts C=O and element C",
+        "ethanol_O": "smarts [CH3][CH2]O and element O",
+        "ethanol_H": "smarts [CH3][CH2]O[H] and smarts [OX2][H] and element H",
+    }
     create_bonds = [("acetic_acid_C", "ethanol_O")]
     delete_atoms = ["acetic_acid_O", "acetic_acid_H", "ethanol_H"]
     rxn = AlchemicalReaction(
