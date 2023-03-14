020d773e6af75109ad12348c4000b5c9e46a38d7
orioncohen <orion@lbl.gov>
2023-03-14 10:30:31 -0700

    refactor alchemy testing

--- pymatgen/io/openmm/tests/test_alchemy_utils.py
+++ pymatgen/io/openmm/tests/test_alchemy_utils.py
@@ -1,48 +1,40 @@
 from pymatgen.io.openmm.alchemy_utils import openff_counts_to_universe
 import openff.toolkit as tk
+import numpy as np
 
 
 class TestAlchemicalReaction:
-    def test_smiles_to_universe(self):
-        O = tk.Molecule.from_smiles("O")
-        CCO = tk.Molecule.from_smiles("CCO")
-        smiles = {O: 2, CCO: 2}
-        universe = openff_counts_to_universe(smiles)
+    def test_openff_counts_to_universe(self):
+        smile_dict = {"O": 2, "CCO": 2}  # water, ethanol, acetic
+        openff_counts = {tk.Molecule.from_smiles(s): n for s, n in smile_dict.items()}
+        universe = openff_counts_to_universe(openff_counts)
         assert len(universe.atoms) == 24
         assert len(universe.atoms.residues) == 4
+        res_lengths = [res.atoms.n_atoms for res in universe.atoms.residues]
+        np.testing.assert_allclose(res_lengths, [3, 3, 9, 9])
 
-    def test_get_reactive_system(
-        self, acetic_ethanol_condensation, acetic_ethanol_condensation_del_water
-    ):
-        O = tk.Molecule.from_smiles("O")
-        CCO = tk.Molecule.from_smiles("CCO")
-        CCOO = tk.Molecule.from_smiles("CC(=O)O")
-        openff_counts = {CCOO: 2, O: 2, CCO: 2}
+    def test_make_acetic_system(self, acetic_rxn, acetic_rxn_rm_water):
+        smile_dict = {"O": 2, "CCO": 2, "CC(=O)O": 2}  # water, ethanol, acetic
+        openff_counts = {tk.Molecule.from_smiles(s): n for s, n in smile_dict.items()}
 
-        # acetic_ethanol_condensation.make_reactive_atoms(openff_counts)
+        acetic_rxn.make_reactive_atoms(openff_counts)
 
-        acetic_ethanol_condensation_del_water.make_reactive_atoms(openff_counts)
-        return
+        acetic_rxn_rm_water.make_reactive_atoms(openff_counts)
         # TODO: add rigorous testing
 
-    def test_visualize_reactions(
-        self, acetic_ethanol_condensation, acetic_ethanol_condensation_del_water
-    ):
-        O = tk.Molecule.from_smiles("O")
-        CCO = tk.Molecule.from_smiles("CCO")
-        CCOO = tk.Molecule.from_smiles("CC(=O)O")
-        openff_mols = [O, CCO, CCOO]
-        # acetic_ethanol_condensation_del_water.visualize_reactions(openff_mols)
-        acetic_ethanol_condensation.visualize_reactions(openff_mols)
+    def test_visualize_reactions(self, acetic_rxn, acetic_rxn_rm_water):
+        smiles = ["O", "CCO", "CC(=O)O"]  # water, ethanol, acetic
+        openff_mols = [tk.Molecule.from_smiles(s) for s in smiles]
+        # acetic_rxn_rm_water.visualize_reactions(openff_mols)
+        rdmol = acetic_rxn.visualize_reactions(openff_mols)
 
-    def test_propanedioic(self, propanedioic_ethanol_condensation_del_water):
-        O = tk.Molecule.from_smiles("O")
-        CCO = tk.Molecule.from_smiles("CCO")
-        CCOO = tk.Molecule.from_smiles("C(C(=O)O)C(=O)O")
-        openff_mols = [O, CCO, CCOO]
-        propanedioic_ethanol_condensation_del_water.visualize_reactions(
-            openff_mols,
-        )
+        assert len(rdmol.GetAtoms()) == sum(mol.n_atoms for mol in openff_mols)
+
+    def test_make_propanedioic_system(self, mols_dict, propanedioic_rxn_rm_water):
+        # water, ethanol, propanedioic
+        smiles = ["O", "CCO", "C(C(=O)O)C(=O)O"]  # water, ethanol, propanedioic
+        openff_mols = [tk.Molecule.from_smiles(s) for s in smiles]
+        propanedioic_rxn_rm_water.visualize_reactions(openff_mols)
         # TODO: add rigorous testing
 
 
@@ -60,4 +52,14 @@ class TestReactiveSystem:
         return
 
     def test_generate_topology(self):
+        return
+
+
+class TestReactiveAtoms:
+    def test_remap(self):
+        return
+
+
+class TestHalfReaction:
+    def test_remap(self):
         return
