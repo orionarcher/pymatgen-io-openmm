f0c6ca51a1457330c54596e1eca1ea1343c26c4a
orioncohen <orion@lbl.gov>
2023-03-14 13:06:16 -0700

    add more robust test for make_acetic_atoms

--- pymatgen/io/openmm/tests/test_alchemy_utils.py
+++ pymatgen/io/openmm/tests/test_alchemy_utils.py
@@ -13,13 +13,36 @@ class TestAlchemicalReaction:
         res_lengths = [res.atoms.n_atoms for res in universe.atoms.residues]
         np.testing.assert_allclose(res_lengths, [3, 3, 9, 9])
 
-    def test_make_acetic_system(self, acetic_rxn, acetic_rxn_rm_water):
-        smile_dict = {"O": 2, "CCO": 2, "CC(=O)O": 2}  # water, ethanol, acetic
+    def test_make_acetic_atoms(self, acetic_rxn, acetic_rxn_rm_water):
+        smile_dict = {"O": 2, "CCO": 5, "CC(=O)O": 2}  # water, ethanol, acetic
         openff_counts = {tk.Molecule.from_smiles(s): n for s, n in smile_dict.items()}
 
-        acetic_rxn.make_reactive_atoms(openff_counts)
+        # for testing purposes
+        mda_atoms = openff_counts_to_universe(openff_counts).atoms
 
-        acetic_rxn_rm_water.make_reactive_atoms(openff_counts)
+        reactive_atoms_1 = acetic_rxn.make_reactive_atoms(openff_counts)
+        trig_left = reactive_atoms_1.trigger_atoms_left
+        trig_right = reactive_atoms_1.trigger_atoms_right
+        assert len(trig_left) == 2
+        assert len(trig_right) == 5
+        assert np.all(mda_atoms[trig_left].elements == "C")
+        assert np.all(mda_atoms[trig_right].elements == "O")
+        trigger_3 = reactive_atoms_1.trigger_atoms_right[3]
+        half_reaction = reactive_atoms_1.half_reactions[trigger_3]
+        assert len(half_reaction.delete_bonds) == 1
+        assert len(half_reaction.create_bonds) == 2
+
+        reactive_atoms_2 = acetic_rxn_rm_water.make_reactive_atoms(openff_counts)
+        trig_left = reactive_atoms_2.trigger_atoms_left
+        trig_right = reactive_atoms_2.trigger_atoms_right
+        assert len(trig_left) == 2
+        assert len(trig_right) == 5
+        assert np.all(mda_atoms[trig_left].elements == "C")
+        assert np.all(mda_atoms[trig_right].elements == "O")
+        trigger = reactive_atoms_2.trigger_atoms_left[1]
+        half_reaction = reactive_atoms_2.half_reactions[trigger]
+        assert len(half_reaction.delete_atoms) == 2
+        assert len(half_reaction.create_bonds) == 1
         # TODO: add rigorous testing
 
     def test_visualize_reactions(self, acetic_rxn, acetic_rxn_rm_water):
@@ -30,7 +53,7 @@ class TestAlchemicalReaction:
 
         assert len(rdmol.GetAtoms()) == sum(mol.n_atoms for mol in openff_mols)
 
-    def test_make_propanedioic_system(self, mols_dict, propanedioic_rxn_rm_water):
+    def test_make_propanedioic_system(self, propanedioic_rxn_rm_water):
         # water, ethanol, propanedioic
         smiles = ["O", "CCO", "C(C(=O)O)C(=O)O"]  # water, ethanol, propanedioic
         openff_mols = [tk.Molecule.from_smiles(s) for s in smiles]
