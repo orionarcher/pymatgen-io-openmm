714508bf65a17d13f3a4c1f4f1fc8d849f7c94f3
orioncohen <orion@lbl.gov>
2023-03-14 21:07:34 -0700

    update fixture names

--- pymatgen/io/openmm/tests/conftest.py
+++ pymatgen/io/openmm/tests/conftest.py
@@ -8,7 +8,7 @@ from pymatgen.io.openmm.alchemy_utils import AlchemicalReaction
 
 
 @pytest.fixture
-def acetic_ethanol_condensation():
+def acetic_rxn():
     """
     A fixture for an acetic acid and ethanol condensation Alchemical Reaction where
     the water is kept.
@@ -22,16 +22,15 @@ def acetic_ethanol_condensation():
     }
     create_bonds = [("acetic_acid_C", "ethanol_O"), ("acetic_acid_O", "ethanol_H")]
     delete_bonds = [("ethanol_O", "ethanol_H"), ("acetic_acid_O", "acetic_acid_C")]
-    rxn = AlchemicalReaction(
+    return AlchemicalReaction(
         select_dict=select_dict,
         create_bonds=create_bonds,
         delete_bonds=delete_bonds,
     )
-    return rxn
 
 
 @pytest.fixture
-def acetic_ethanol_condensation_del_water():
+def acetic_rxn_rm_water():
     """
     A fixture for an acetic acid and ethanol condensation Alchemical Reaction where
     the water is deleted.
@@ -45,32 +44,30 @@ def acetic_ethanol_condensation_del_water():
     }
     create_bonds = [("acetic_acid_C", "ethanol_O")]
     delete_atoms = ["acetic_acid_O", "acetic_acid_H", "ethanol_H"]
-    rxn = AlchemicalReaction(
+    return AlchemicalReaction(
         select_dict=select_dict,
         create_bonds=create_bonds,
         delete_atoms=delete_atoms,
     )
-    return rxn
 
 
 @pytest.fixture
-def propanedioic_ethanol_condensation_del_water():
+def propanedioic_rxn_rm_water():
     """
     A fixture for an acetic acid and ethanol condensation Alchemical Reaction where
     the water is deleted.
     """
     select_dict = {
         "propanedioic_acid_O": "smarts OC(=O)CC(O)=O and smarts C-O and element O",
-        "propanedioic_acid_H": "smarts OC(=O)CC(O)=O and smarts [OX2][H] and element H",
+        "propanedioic_acid_H": "(byres smarts OC(=O)CC(O)=O) and smarts [OX2][H] and element H",
         "propanedioic_acid_C": "smarts OC(=O)CC(O)=O and smarts C=O and element C",
         "ethanol_O": "smarts [CH3][CH2]O and element O",
         "ethanol_H": "smarts [CH3][CH2]O[H] and smarts [OX2][H] and element H",
     }
     create_bonds = [("propanedioic_acid_C", "ethanol_O")]
     delete_atoms = ["propanedioic_acid_O", "propanedioic_acid_H", "ethanol_H"]
-    rxn = AlchemicalReaction(
+    return AlchemicalReaction(
         select_dict=select_dict,
         create_bonds=create_bonds,
         delete_atoms=delete_atoms,
     )
-    return rxn
